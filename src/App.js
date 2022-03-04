a457b6ce500b0261507d04da750e969b99988315
Shaheen-Siddiqui <95218912+Shaheen-Siddiqui@users.noreply.github.com>
2022-03-04 22:48:54 +0530

    last commit gusse me

--- src/App.js
+++ src/App.js
@@ -1,20 +1,82 @@
-// import ReactuseMediaQuery from 'react'
-// import useMediaQuery from 'use-media-query-hook';
-// import MobileView from './MobileView'
-// import PCView from './PCView';
+import React from 'react'
 
-// const App = () => {
-//   const ismobileview = useMediaQuery('(max-width: 924px)');
-//   return (
-//     <>
-//       {
-//     ismobileview ?
-//     <PCView> :<MobileView> 
+const App = () => {
+  return (
+    <>
+      <div className="body">
+<header className='Header'>
+        <div className="innerRound">
+        <div className="ImgIcon">
+          <div className=""><img className='forH_w' src="https://i.pinimg.com/736x/8e/19/2f/8e192f3f6c11504bcd664c39d2fd0d78.jpg" alt="" /> </div>
+          <p className='sayaStrong'><strong>Saya</strong> </p>
+          <div className=""><i className='fas fa-shopping-cart'></i></div>
+          </div>
+          </div>
+          <div className="Inputfild">
+         <div className="">  <input placeholder='Search' className='innerinput' type="search" name="search"/></div>
+         <div className=""> <i className="fa fa-search" style={{fontSize:"24px"}}></i></div>
+        </div>
+</header>
 
-//    }  
+<div className="EmpatydiV"></div>
+  <div className="OuterOrderMeds">
+  <div className="OrderMedsFild"><p className='textSet'>ORDER MEDS</p> </div>
+  <div className="OrderMedsFild"><p className='textSet'>Your Prescription Transcribe</p> </div>
+</div>
 
-//     </>
-//   )
-// }
+<div className="ProtienOuter">
+  <div className="outerImageDiv">
+  <p className=' firstP'>Jan Aushsdhi <strong>PROTEIN</strong> </p>
+  <img className=' FirstImage' src="https://previews.123rf.com/images/doomu/doomu1707/doomu170700091/83022270-milk-carton-boxes-on-a-white-background-3d-rendering.jpg" alt="" /> 
+  </div>
+  <div className="OuterDoubleDiv">
+    <div className="inerDoubleDiv">
+    <img className='forH__W SecImage' src="https://images.albertsons-media.com/is/image/ABS/960230468?$ecom-pdp-desktop$&defaultImage=Not_Available" alt="" />
+      <p className='PeradeS secP'>Jan Aushsdhi <strong>PROTEIN</strong></p>
+      <p className='PeradeS thirdP'>(KESAR PASTA)</p>
+    </div>
+    <div className="inrDoubleSec">
+    <img className='forH__W S ThirdImage' src="https://i5.walmartimages.com/asr/8935e1f2-dcb4-4e4f-9277-8398cb2e1046.305a34f33ca92aeeb11d523d4610e5af.jpeg" alt="" />
+    <p className='PeradeS fourP'>Jan Aushsdhi <strong>PROTEIN</strong></p>
+    <p className='PeradeS fiveP'>(VENILLA)</p>
+    </div>
+  </div>
+</div>
 
-// export default App;
+<div className="trioHandler">
+  <div className="forTrioH_W"><img className='forImgH_W' src="https://cdn.shopify.com/s/files/1/0011/9904/6723/products/CK-133-1200900_700x700.jpg?v=1571785692" alt="heartImage" /> <p className='SecPera'>BLOOD PRESSURE</p> </div>
+  <div className="forTrioH_W"><img className='forImgH_W' src="https://pixy.org/download/4326546/" alt="heartImage" /> <p className='SecPera'>HEAT DISEASE</p></div>
+  <div className="forTrioH_W"><img className='forImgH_W' src="https://cdn.mos.cms.futurecdn.net/NA8RHrNKQYk4PNs9YzUZY9-320-80.jpg" alt="" /> <p className='SecPera'>DIABLETES</p> </div>
+</div>
+
+ <div className="body1">
+      <img src="https://m.media-amazon.com/images/I/61PF3g4c8NL._SL1100_.jpg" alt="" className='img_body' />
+  </div>
+
+<div className="outerMain">
+  <div ><img className="LeftSideImg"  src="https://thumbs.dreamstime.com/b/blood-glucose-meter-strips-isolated-white-background-d-illustration-145084332.jpg" alt="" /> </div>
+  <div className="perastyling">
+  <div className="ForPera">   <p className='glu-title'>Glucometer Digital</p>
+    <p>(Blood Glucose Monitor|Test strips <br /> | control Solution | Sterile lancets |<br /> Lancing Device)</p>
+    <p className='Rupees'>Rs. 500/- </p>
+  </div>
+</div>
+</div>
+
+<div className="FourGroupdiv">
+ <img className='ImageLast' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrWcvGNfNQmM8ohqYWwUF4SsStGkY3E8nE8g&usqp=CAU" alt="" />
+ <img className='ImageLast' src="https://www.ubuy.co.in/productimg/?image=aHR0cHM6Ly9tLm1lZGlhLWFtYXpvbi5jb20vaW1hZ2VzL0kvNzFubnhNN2tnUEwuX0FDX1NMMTUwMF8uanBn.jpg" alt="" />
+ <img className='ImageLast' src="https://m.media-amazon.com/images/I/61ha-SjTJqL._SL1000_.jpg" alt="" />
+ <img className='ImageLast' src="https://5.imimg.com/data5/PX/RC/MY-49324331/natural-mango-juice-250x250.jpg" alt="" />
+ <img className='ImageLast' src="https://wa-kaimono-backend-tokyo-production.s3.ap-northeast-1.amazonaws.com/gazou/products/4987493001324/4987493001324_01.jpg?1568960817" alt="" />
+ 
+</div>
+</div>
+
+
+
+    </>
+  )
+}
+
+export default App;
